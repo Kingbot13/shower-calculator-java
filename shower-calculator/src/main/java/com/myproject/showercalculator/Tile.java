26247f4d09fe26e00b30213c80ed1f1d79f0ed3a
Dylan King <dwkblue51@yahoo.com>
2022-11-08 15:48:34 -0600

    initial commit

---
+++ shower-calculator/src/main/java/com/myproject/showercalculator/Tile.java
@@ -0,0 +1,16 @@
+package com.myproject.showercalculator;
+
+public class Tile {
+	int length;
+	int width;
+	double thickness;
+	double carton;
+	double price;
+	public Tile(int length, int width, double thickness, double carton, double price) {
+		this.length = length;
+		this.width = width;
+		this.thickness = thickness;
+		this.carton = carton;
+		this.price = price;
+	}
+}
