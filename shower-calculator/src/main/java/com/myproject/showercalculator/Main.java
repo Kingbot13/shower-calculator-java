ada6a3d02ab133f7ca3caa563026ff55b71c262e
Dylan King <dwkblue51@yahoo.com>
2022-11-10 14:31:47 -0600

    create new tile object

--- shower-calculator/src/main/java/com/myproject/showercalculator/Main.java
+++ shower-calculator/src/main/java/com/myproject/showercalculator/Main.java
@@ -3,7 +3,12 @@ package com.myproject.showercalculator;
 public class Main {
 
 	public static void main(String[] args) {
-		new GUI();
+		GUI gui = new GUI();
+		// TODO add price and carton size for tiles
+		if(gui.tileLength != 0 && gui.tileWidth != 0) {
+			Tile tile = new Tile(gui.tileLength, gui.tileWidth, gui.tileThickness, 0.0, 0.0);
+			
+		}
 	}
 
 }
