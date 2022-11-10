bf1a0d3458af2b2c5561532c827112094609d6cc
Dylan King <dwkblue51@yahoo.com>
2022-11-10 15:35:37 -0600

    create squareFootage method

--- shower-calculator/src/main/java/com/myproject/showercalculator/Calculator.java
+++ shower-calculator/src/main/java/com/myproject/showercalculator/Calculator.java
@@ -1,5 +1,7 @@
 package com.myproject.showercalculator;
 
 public class Calculator {
-
+	public static double squareFootage(double side, double back, double height) {
+		return side * height * 2 + back * height; 
+	}
 }
