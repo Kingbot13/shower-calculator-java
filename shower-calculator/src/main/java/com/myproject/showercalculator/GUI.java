881d30608e22bec7e6ecd3fc8eae4acc56024e6b
Dylan King <dwkblue51@yahoo.com>
2022-11-10 15:15:36 -0600

    add text fields for shower dimensions

--- shower-calculator/src/main/java/com/myproject/showercalculator/GUI.java
+++ shower-calculator/src/main/java/com/myproject/showercalculator/GUI.java
@@ -11,9 +11,20 @@ public class GUI implements ActionListener {
 	private JTextField tWidth;
 	private JButton button;
 	private JTextField tThickness;
+	private JTextField tCarton;
+	private JTextField tPrice;
+	private JTextField tsSide;
+	private JTextField tsBack;
+	private JTextField tsHeight;
 	public int tileLength;
 	public int tileWidth;
 	public double tileThickness;
+	public double carton;
+	public double price;
+	public double sideLength;
+	public double backLength;
+	public double height;
+	
 	
 	public GUI() {
 		frame = new JFrame("Shower Calculator");
@@ -22,6 +33,11 @@ public class GUI implements ActionListener {
 		tLength = new JTextField(2); // tile length text field
 		tWidth = new JTextField(2); // tile width text field
 		tThickness = new JTextField(4); // tile thickness text field
+		tCarton = new JTextField(4);
+		tPrice = new JTextField(4);
+		tsSide = new JTextField(4);
+		tsBack = new JTextField(4);
+		tsHeight = new JTextField(4);
 		button = new JButton("Submit");
 		
 		// add components to frame
@@ -38,6 +54,12 @@ public class GUI implements ActionListener {
 		tileLength = Integer.parseInt(tLength.getText());
 		tileWidth = Integer.parseInt(tWidth.getText());
 		tileThickness = Double.parseDouble(tThickness.getText());
+		carton = Double.parseDouble(tCarton.getText());
+		price = Double.parseDouble(tPrice.getText());
+		sideLength = Double.parseDouble(tsSide.getText());
+		backLength = Double.parseDouble(tsBack.getText());
+		height = Double.parseDouble(tsHeight.getText());
+		
 		
 	}
 }
