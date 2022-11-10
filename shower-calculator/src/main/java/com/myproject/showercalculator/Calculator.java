ff4d32d8ce729a621f62f3856bc7d67b74ec7977
Dylan King <dwkblue51@yahoo.com>
2022-11-10 15:49:13 -0600

    add calculator methods

--- shower-calculator/src/main/java/com/myproject/showercalculator/Calculator.java
+++ shower-calculator/src/main/java/com/myproject/showercalculator/Calculator.java
@@ -1,7 +1,17 @@
 package com.myproject.showercalculator;
 
 public class Calculator {
+	public static double sf;
+	public static double totalCartons;
 	public static double squareFootage(double side, double back, double height) {
-		return side * height * 2 + back * height; 
+		return sf = side * height * 2 + back * height; 
+	}
+	
+	public static double cartons(double carton) {
+		return totalCartons = sf / carton;
+	}
+	
+	public static double price(double p) {
+		return totalCartons * p;
 	}
 }
