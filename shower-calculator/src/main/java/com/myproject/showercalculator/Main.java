ad56e1b58306db7f0aba139b95bddc8acbf7db10
Dylan King <dwkblue51@yahoo.com>
2022-11-10 15:29:27 -0600

    define shower object

--- shower-calculator/src/main/java/com/myproject/showercalculator/Main.java
+++ shower-calculator/src/main/java/com/myproject/showercalculator/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		GUI gui = new GUI();
-		// TODO add price and carton size for tiles
+		Tile tile;
+		Shower shower;
 		if(gui.tileLength != 0 && gui.tileWidth != 0) {
-			Tile tile = new Tile(gui.tileLength, gui.tileWidth, gui.tileThickness, 0.0, 0.0);
-			
+			tile = new Tile(gui.tileLength, gui.tileWidth, gui.tileThickness, gui.carton, gui.price);
+		}
+		if(gui.backLength != 0.0 && gui.sideLength != 0.0 && gui.height != 0.0) {
+			shower = new Shower(gui.sideLength, gui.backLength, gui.height);
 		}
 	}
 
