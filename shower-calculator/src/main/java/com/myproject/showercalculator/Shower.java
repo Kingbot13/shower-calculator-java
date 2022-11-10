e96e20c63031f0c56b9e9b758a5f44ffc73de65f
Dylan King <dwkblue51@yahoo.com>
2022-11-10 14:48:00 -0600

    create shower class

---
+++ shower-calculator/src/main/java/com/myproject/showercalculator/Shower.java
@@ -0,0 +1,14 @@
+package com.myproject.showercalculator;
+
+public class Shower {
+	public double sideLength;
+	public double backLength;
+	public double height;
+	
+	Shower(double side, double back, double h) {
+		this.sideLength = side;
+		this.backLength = side;
+		this.height = h;
+	}
+	
+}
